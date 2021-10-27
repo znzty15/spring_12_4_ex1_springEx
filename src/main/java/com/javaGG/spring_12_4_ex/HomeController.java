223cd87fb293deeb91d7c855a366c8e5df3ed5dc
GoRyeoIn <89904365+znzty15@users.noreply.github.com>
2021-10-27 11:50:50 +0900

    controller Ex

--- src/main/java/com/javaGG/spring_12_4_ex/HomeController.java
+++ src/main/java/com/javaGG/spring_12_4_ex/HomeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-@RequestMapping("/board")
+@RequestMapping("/boad")
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
